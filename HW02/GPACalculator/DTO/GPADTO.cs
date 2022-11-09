11eb73796d6e9315dac6204f134efec02da9a18f
talhaejaz539 <talhaejazup@gmail.com>
2022-11-09 20:19:25 +0500

    HomeWork02

---
+++ HW02/GPACalculator/DTO/GPADTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class GPADTO
+    {
+        public string Name { get; set; }
+
+        public string RollNumber { get; set; }
+
+        public string SubjectTitle { get; set; }
+
+        public int Marks { get; set; }
+
+        public string Grade { get; set; }
+
+        public string GPA { get; set; }
+
+    }
+}
---
+++ HW02/GPACalculator/GPACalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PL;
+
+namespace GPACalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t\t\t\tGPA CALCULATOR\n\n");
+            int input;
+
+            do
+            {
+                Console.WriteLine("\n\t1- Enter Student Data");
+                Console.WriteLine("\t2- Output Student Data");
+                Console.WriteLine("\t3- Exit");
+                Console.Write("\t\tPlease select one of the above options: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    GPAView gpa = new GPAView();
+                    gpa.InputData();
+                }
+
+                else if(input == 2)
+                {
+                    GPAView gpa = new GPAView();
+                    gpa.OutputData();
+                    Console.WriteLine("\tPress any key to go to main menu.......");
+                    Console.ReadKey(true);
+                }
+
+            } while (input != 3);
+        }
+    }
+}
